44e03c0edd7fc058d913cef0d2c5a12aaae51acb
William Okafor <wilpat456@gmail.com>
2019-03-03 12:52:39 +0100

    Added function and tests for delegated token transfer

--- contracts/BaToken.sol
+++ contracts/BaToken.sol
@@ -14,13 +14,26 @@ contract BaToken {
   // Set the standard
   string public standard = 'BaToken v1.0'; // Not in erc20 standard
 
-  mapping (address => uint) public balanceOf; // Track the tokens belonging to each address that has received a transfer
+  mapping (address => uint256) public balanceOf; // Track the tokens belonging to each address that has received a transfer
   
+  // Emit that a transfer happened
   event Transfer(
       address indexed _from,
       address indexed _to,
-      uint _value
+      uint256 _value
   );
+
+  // Emit that a delegated spending has been approved
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+
+  // Two levels deep mapping -- From left to right means: I, address a, permits address b to spend x amount of tokens on my behalf
+  // This was, a single address can delegate token spendings to multiple addresses
+  mapping(address => mapping(address => uint256)) public allowance;
+  
 
   constructor (uint256 _initialSupply) public{
     totalSupply = _initialSupply;
@@ -29,7 +42,7 @@ contract BaToken {
   }
 
   // Transfer function -- compulsory in erc20
-  function transfer (address _to, uint _value) public returns(bool res) {
+  function transfer (address _to, uint256 _value) public returns(bool res) {
 
     // Run check
     require (balanceOf[msg.sender] >= _value);
@@ -45,6 +58,27 @@ contract BaToken {
     return true;
     
   }
+  
+  function approve (address _spender, uint256 _value) public returns(bool res) {
+    
+    // allowance
+    allowance[msg.sender][_spender] = _value;
+    //Emit the Approval event
+    emit Approval(msg.sender, _spender, _value);
+
+    return true;
+  }
+
+  function transferFrom (address _owner, address _to, uint256 _value) public returns(bool res) {
+    require(balanceOf[_owner] >= _value); // the owner's balance can take this
+    require(allowance[_owner][msg.sender] >= _value); // the allowed delegated transfer isnt crossed
+    emit Transfer(_owner, _to, _value);
+    balanceOf[_owner] -= _value;
+    balanceOf[_to] += _value;
+    allowance[_owner][msg.sender] -= _value;
+    return true;
+  }
+  
   
 
 }
--- test/BaToken.js
+++ test/BaToken.js
@@ -53,5 +53,69 @@ contract('BaToken', (accounts) => {
 		}).then(balance => {
 			assert.equal(balance.toNumber(), 750000, 'Deducts the sent amount from sender')
 		});
+	});
+
+	it('approves tokens for delegated transfer', () => {
+		return BaToken.deployed().then(i => {
+		 app = i;
+		 return app.approve.call(accounts[1], 100) // .call doesnt actually create a transaction
+		}).then(res => {
+			assert.equal(res, true, 'Delegated transfer returns true')
+			return app.approve(accounts[1], 100)
+		}).then(receipt => {
+			assert.equal(receipt.logs.length, 1, 'Triggers one event');
+			assert.equal(receipt.logs[0].event, 'Approval', 'Should be the "Transfer" event');
+			assert.equal(receipt.logs[0].args._owner, accounts[0], 'Logs the account that the tranfer was from');
+			assert.equal(receipt.logs[0].args._spender, accounts[1], 'Logs the account that the transfer was to');
+			assert.equal(receipt.logs[0].args._value, 100, 'Logs the transfer amount');
+			return app.allowance(accounts[0], accounts[1])
+		}).then(allowance => {
+			assert.equal(allowance.toNumber(), 100, 'Stores the allowance for delegated transfer');
+		})
+	});
+
+	it('handles the actual token transfer delegation', () => {
+		return BaToken.deployed().then(i => {
+		 app = i;
+
+		 owner = accounts[2]; // Because account 1 has received funds previously
+		 spender = accounts[3];
+		 to = accounts[4]
+		 return app.transfer(owner, 100, {from : accounts[0]})
+		}).then(receipt => {
+		 return app.approve(spender, 10, { from: owner}) 
+		}).then(receipt =>{
+			//Try transferring larger than the owner's balance
+			return app.transferFrom(owner, to, 9999, {from: spender})
+		}).then(assert.fail).catch(error =>{
+			assert(error.message.indexOf('revert') >= 0, 'Cannot transfer more than owner\'s balance');
+			// Try transferring more than the allocated delegation
+			return app.transferFrom(owner, to, 20, {from: spender})
+		}).then(assert.fail).catch(error =>{
+			assert(error.message.indexOf('revert') >= 0, 'Cannot transfer more than delegated allowance');
+			// Check for boolean response on transfer of the accepted amount 
+			return app.transferFrom.call(owner, to, 10, {from: spender})
+		}).then(res => {
+			assert.equal(res, true, 'Delegated transfer was successful');
+			// Try actual transfer
+			return app.transferFrom(owner, to, 10, {from: spender})
+		}).then(receipt =>{
+			assert.equal(receipt.logs.length, 1, 'One event was triggered');
+			assert.equal(receipt.logs[0].event, 'Transfer', 'Should be the "Transfer" event');
+			assert.equal(receipt.logs[0].args._from, owner, 'Logs the account that the tranfer was from');
+			assert.equal(receipt.logs[0].args._to, to, 'Logs the account that the transfer was to');
+			assert.equal(receipt.logs[0].args._value, 10, 'Logs the transfer amount');
+			// Check owner's balance
+			return app.balanceOf(owner)
+		}).then(bal => {
+			assert.equal(bal.toNumber(), 90, 'Deducted the transfered token from the sender');
+			// Check receiver's balance
+			return app.balanceOf(to);
+		}).then(bal =>{
+			assert.equal(bal.toNumber(), 10, 'Adds the transfered to the receiver');
+			return app.allowance(owner, spender);
+		}).then(allowance => {
+			assert.equal(allowance.toNumber(), 0, 'successfully deducts the allowance')
+		})
 	})
 })
